87ce52c0aaff57bb6d6fa39f725ebe81279ff9b5
Heitor Souza <heitorhenriquedias@gmail.com>
2020-04-04 13:17:48 -0300

    132004042020

--- whatsapp/js/utils/Events.js
+++ whatsapp/js/utils/Events.js
@@ -11,9 +11,9 @@ class Events {
 
     initEvents() {
 
-       this.profileEvents();
-       this.contactsEvents();
-       this.btnAttachEvents();
+        this.profileEvents();
+        this.contactsEvents();
+        this.btnAttachEvents();
 
     }
     //.initEvents
@@ -77,6 +77,7 @@ class Events {
     contactsEvents() {
 
         this.elList.btnNewContact.on("click", () => {
+            // nova conversa
 
             this.closeLeftSidePanel();
 
@@ -92,6 +93,7 @@ class Events {
         //.btnNewContact
 
         this.elList.btnClosePanelAddContact.on("click", () => {
+            // fechar nova conversa
 
             this.elList.panelAddContact.hide();
 
@@ -101,6 +103,7 @@ class Events {
         //.btnClosePanelAddContact
 
         this.elList.formPanelAddContact.on("submit", (e) => {
+            // botão submit nova conversa
 
             e.preventDefault();
 
@@ -110,6 +113,7 @@ class Events {
         // .formPanelAddContact
 
         this.elList.contactsMessagesList.querySelectorAll(".contact-item").forEach(contact => {
+            //clique em um contato na lista de contatos.
 
             contact.on("click", () => {
 
@@ -132,45 +136,141 @@ class Events {
     btnAttachEvents() {
 
         this.elList.btnAttach.on("click", (e) => {
+            //botão anexar
 
             e.stopPropagation();
+            // executa somente nessa camada. Os elementos ancestrais do btnAttach não vão receber os efeitos colaterais da função
 
             this.elList.menuAttach.addClass("open");
 
             document.addEventListener("click", this.closeMenuAttach.bind(this));
+            // Utilizando o bind para poder manter o escopo do this como a classe Event
+            // dentro do document o this passaria a ser o próprio document, porém não queremos isso. Pra manter o escopo na classe Event usamos o bind.
 
         });
         // .btnAttach
 
+        this.btnAttachPhotoEvents();
+
+        this.btnAttachCameraEvents();
+
+        this.btnAttachDocumentEvents();
+
+        this.btnAttachContactEvents();
+
+    }
+    // .btnAttachEvents
+
+    btnAttachPhotoEvents() {
+
         this.elList.btnAttachPhoto.on("click", () => {
 
-            console.log("Photo");
+            this.elList.inputPhoto.click();
 
         });
         // .btnAttachPhoto
 
+        this.elList.inputPhoto.on("change", () => {
+
+            //a lista de arquivos selectionados no input file é uma coleção que não pode ser iterada pelo forEach. Para percorrê-la temos que converter em array
+            Array.from(this.elList.inputPhoto.files).forEach(file => {
+
+                console.log(file);
+
+            });
+
+        });
+        // .inputPhoto
+
+    }
+    // .btnAttachPhotoEvents
+
+    btnAttachCameraEvents() {
+
         this.elList.btnAttachCamera.on("click", () => {
 
-            console.log("Camera");
+            this.closeAllMainPanel();
+
+            this.elList.panelCamera.css({
+                height: 'calc(100% - 120px)'
+            });
+
+            this.elList.panelCamera.addClass('open');
 
         });
         // .btnAttachCamera
 
+        this.elList.btnClosePanelCamera.on('click', () => {
+
+            this.closeAllMainPanel();
+
+            this.elList.panelMessagesContainer.show();
+
+        });
+        // .btnClosePanelCamera
+
+        this.elList.btnTakePicture.on('click', () => {
+
+            console.log('click-zum');
+
+        });
+        // .btnTakePicture
+
+    }
+    // .btnAttachCameraEvents
+
+    btnAttachDocumentEvents() {
+
         this.elList.btnAttachDocument.on("click", () => {
 
-            console.log("Document");
+            this.closeAllMainPanel();
+
+            this.elList.panelDocumentPreview.addClass('open');
+
+            this.elList.panelDocumentPreview.css({
+                height: 'calc(100% - 110px)'
+            });
 
         });
         // .btnAttachDocument
 
+        this.elList.btnClosePanelDocumentPreview.on("click", () => {
+
+            this.closeAllMainPanel();
+
+            this.elList.panelMessagesContainer.show();
+
+        });
+        // .btnClosePanelDocumentPreview
+
+        this.elList.btnSendDocument.on("click", () => {
+
+            console.log("Sending documents");
+
+        });
+        // .btnSendDocument
+
+    }
+    // .btnAttachDocumentsEvents
+
+    btnAttachContactEvents() {
+
         this.elList.btnAttachContact.on("click", () => {
 
-            console.log("Contact");
+            this.elList.modalContacts.show();
 
         });
         // .btnAttachContact
+
+        this.elList.btnCloseModalContacts.on("click", () => {
+
+            this.elList.modalContacts.hide();
+
+        });
+        // .btnCloseModalContacts
+
     }
-    // .btnAttachEvents
+    // .btnAttachContactEvents
 
     closeLeftSidePanel() {
 
@@ -189,6 +289,15 @@ class Events {
 
     }
     // .closeMenuAttach
+
+    closeAllMainPanel() {
+
+        this.elList.panelMessagesContainer.hide();
+        this.elList.panelCamera.removeClass('open');
+        this.elList.panelDocumentPreview.removeClass('open');
+
+    }
+    // .closeAllMainPanel
 
 }
 //.Events
